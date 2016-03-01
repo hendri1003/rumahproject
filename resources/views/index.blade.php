8c3218f7603ba82eb7a713360e38496d16dc97a3
genbox1 <genbox1@hendri1003@gmail.com>
2016-03-01 12:04:20 +0700

    second commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,42 +7,106 @@
 	{!! Html::style('css/font-awesome.min.css')!!}
 	{!! Html::style('css/styles.css')!!}
 
-	{!! Html::script('js/jquery-1.12.1.min.js')!!}
+	{!! Html::script('js/jquery-1.11.0.min.js')!!}
   {!! Html::script('js/bootstrap.min.js')!!}
   {!! Html::script('js/jquery-latest.pack.js')!!}
   {!! Html::script('js/jcarousellite_1.0.1c4.js')!!}
 
   <script type="text/javascript">
-  $(function() {
+  $(document).ready(function() {
+    // slider jumbotron
     $(".event-items").jCarouselLite({
       vertical: true,
       hoverPause:true,
       visible: 1,
       auto:500,
       speed:1000
+    }); // end slider jumbotron
+
+    // slider account and signup
+    $('#login-trigger').click(function(e) {
+      e.stopPropagation();
+      $('#signup-content').slideUp();
+      $(this).next('#login-content').slideToggle('fast');
     });
+    $('#signup-trigger').click(function(e) {
+      e.stopPropagation();
+      $('#login-content').slideUp();
+      $(this).next('#signup-content').slideToggle('fast');
+    }); // end slider account and signup
   });
+
+  // slideup account and signup
+  $(document).click(function() {
+    $('#login-content, #signup-content').bind('click', function(e){return false});
+    $('#login-content, #signup-content').slideUp();
+  }); // end slideup account and signup
   </script>
 </head>
 <body>
 	<div class="container-fluid">
 		<nav class="navbar navbar-inverse navbar-fixed-top">
 			<div class="wrap">
+        <!-- Menu Top -->
 				<div id="menu-top">
 					<ul>
 						<li><a href="#">Home</a></li>
 						<li><a href="#">About</a></li>
 						<li><a href="#">Contact</a></li>
 					</ul>
-				</div>
-        <div id="header-right">
+				</div> <!-- End Menu Top -->
+        <!-- Menu Top Right -->
+        <div id="header-right"> 
+          <div id="account">
+            <!-- login -->
+            <a href="#" id="login-trigger" onclick="return false">
+              <span class="icon"><i class="fa fa-user"></i></span> Account
+            </a>
+            <div id="login-content">
+              <div id="login-input">
+                <div class="form-group">
+                    {!! Form::text('username', null, ['id'=>'username-login-input', 'class'=>'form-control', 'placeholder'=>'Username/email']) !!}
+                </div>
+                <div class="form-group">
+                  {!! Form::password('password', ['id'=>'password-login-input', 'class'=>'form-control', 'placeholder'=>'Password']) !!}
+                </div>
+              </div>
+              <div id="login-button">
+                {!! Form::submit('Login', ['id'=>'login-btn', 'class'=>'btn btn-default']) !!}
+              </div>
+            </div> <!-- end login -->
+            <!-- sign up -->
+            <a href="#" id="signup-trigger" onclick="return false">
+              <span class="icon"><i class="fa fa-sign-in"></i></span> Sign up
+            </a>
+            <div id="signup-content">
+              <div id="signup-input">\
+                <div class="form-group">
+                    {!! Form::text('firstname', null, ['id'=>'firstname-signup-input', 'class'=>'form-control', 'placeholder'=>'firstname']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::text('lastname', null, ['id'=>'lastname-signup-input', 'class'=>'form-control', 'placeholder'=>'lastname']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::text('username', null, ['id'=>'username-signup-input', 'class'=>'form-control', 'placeholder'=>'Username/email']) !!}
+                </div>
+                <div class="form-group">
+                  {!! Form::password('password', ['id'=>'password-signup-input', 'class'=>'form-control', 'placeholder'=>'Password']) !!}
+                </div>
+              </div>
+              <div id="login-button">
+                {!! Form::submit('Sign up', ['id'=>'signup-btn', 'class'=>'btn btn-default']) !!}
+              </div>
+            </div> <!-- end sign up -->
+          </div>
           <div id="form-search">
             <?php echo Form::text('search', null, ['id'=>'search', 'placeholder'=>'search...']); ?>
             <span class="icon"><i class="fa fa-search"></i></span>
           </div>
-        </div>
+        </div> <!-- End Menu Top Right -->
 			</div>
 		</nav>
+    <!-- Jumbotron bootstrap slider -->
     <div id="carousel-header" class="carousel slide carousel-fade row" data-ride="carousel">
       <div class="carousel-inner">
         <div class="item active">
@@ -55,8 +119,9 @@
           <div class="slide3"></div>
         </div>
       </div>
-    </div>
+    </div> <!-- End Jumbotron bootstrap slider -->
     <div id="content" class="row">
+      <!-- LEFT MENU -->
       <div id="left-menu" class="col-lg-3">
         <div id="news">
           <div class="left-menu-header">News</div>
@@ -71,7 +136,6 @@
             </div>
           </div>
         </div>
-
         <div id="events">
           <div class="left-menu-header">Events</div>
           <div class="left-menu-body">
@@ -96,11 +160,9 @@
                   <div class="event-date">7 Aug 2016, 13pm</div>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
-
         <div id="testimonials">
           <div class="left-menu-header">Testimonials</div>
           <div class="left-menu-body">
@@ -131,13 +193,126 @@
             </div>
           </div>
         </div>
-
-      </div>
+      </div> <!-- END LEFT MENU -->
+      <!-- RIGHT MENU -->
       <div id="right-menu" class="col-lg-9">
-        tes
-        <!-- {{!! Form::image('../img/') !!}} -->
-      </div>
+        <div class="col-lg-4">
+          <div class="img-content">
+            {!! Html::image('../img/item1.jpg') !!}
+          </div>
+          <div class="img-description">
+            <div class="content-title">
+              <div class="content-title-left">Jakarta, Bintaro</div>
+              <div class="content-title-right">7 M</div>
+            </div>
+            <div class="content-description">
+              <div class="land-size content-icon"><span class="icon land-size-icon"><i class="fa fa-arrows"></i></span> 130</div>
+              <div class="building-size content-icon"><span class="icon land-size-icon"><i class="fa fa-building-o"></i></span> 250</div>
+              <div class="bedroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 4</div>
+              <div class="bathroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 3</div>
+              <div class="viewer-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-eye"></i></span> 1300</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="img-content">
+            {!! Html::image('../img/item2.jpg') !!}
+          </div>
+          <div class="img-description">
+            <div class="content-title">
+              <div class="content-title-left">Jakarta, Bintaro</div>
+              <div class="content-title-right">7 M</div>
+            </div>
+            <div class="content-description">
+              <div class="land-size content-icon"><span class="icon land-size-icon"><i class="fa fa-arrows"></i></span> 130</div>
+              <div class="building-size content-icon"><span class="icon land-size-icon"><i class="fa fa-building-o"></i></span> 250</div>
+              <div class="bedroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 4</div>
+              <div class="bathroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 3</div>
+              <div class="viewer-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-eye"></i></span> 1300</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="img-content">
+            {!! Html::image('../img/item3.jpg') !!}
+          </div>
+          <div class="img-description">
+            <div class="content-title">
+              <div class="content-title-left">Jakarta, Bintaro</div>
+              <div class="content-title-right">7 M</div>
+            </div>
+            <div class="content-description">
+              <div class="land-size content-icon"><span class="icon land-size-icon"><i class="fa fa-arrows"></i></span> 130</div>
+              <div class="building-size content-icon"><span class="icon land-size-icon"><i class="fa fa-building-o"></i></span> 250</div>
+              <div class="bedroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 4</div>
+              <div class="bathroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 3</div>
+              <div class="viewer-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-eye"></i></span> 1300</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="img-content">
+            {!! Html::image('../img/item3.jpg') !!}
+          </div>
+          <div class="img-description">
+            <div class="content-title">
+              <div class="content-title-left">Jakarta, Bintaro</div>
+              <div class="content-title-right">7 M</div>
+            </div>
+            <div class="content-description">
+              <div class="land-size content-icon"><span class="icon land-size-icon"><i class="fa fa-arrows"></i></span> 130</div>
+              <div class="building-size content-icon"><span class="icon land-size-icon"><i class="fa fa-building-o"></i></span> 250</div>
+              <div class="bedroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 4</div>
+              <div class="bathroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 3</div>
+              <div class="viewer-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-eye"></i></span> 1300</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="img-content">
+            {!! Html::image('../img/item3.jpg') !!}
+          </div>
+          <div class="img-description">
+            <div class="content-title">
+              <div class="content-title-left">Jakarta, Bintaro</div>
+              <div class="content-title-right">7 M</div>
+            </div>
+            <div class="content-description">
+              <div class="land-size content-icon"><span class="icon land-size-icon"><i class="fa fa-arrows"></i></span> 130</div>
+              <div class="building-size content-icon"><span class="icon land-size-icon"><i class="fa fa-building-o"></i></span> 250</div>
+              <div class="bedroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 4</div>
+              <div class="bathroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 3</div>
+              <div class="viewer-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-eye"></i></span> 1300</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="img-content">
+            {!! Html::image('../img/item3.jpg') !!}
+          </div>
+          <div class="img-description">
+            <div class="content-title">
+              <div class="content-title-left">Jakarta, Bintaro</div>
+              <div class="content-title-right">7 M</div>
+            </div>
+            <div class="content-description">
+              <div class="land-size content-icon"><span class="icon land-size-icon"><i class="fa fa-arrows"></i></span> 130</div>
+              <div class="building-size content-icon"><span class="icon land-size-icon"><i class="fa fa-building-o"></i></span> 250</div>
+              <div class="bedroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 4</div>
+              <div class="bathroom-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-bed"></i></span> 3</div>
+              <div class="viewer-numbers content-icon"><span class="icon land-size-icon"><i class="fa fa-eye"></i></span> 1300</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <div id="ads-banner">
+            <a href="#">{!! Html::image('../img/ads1.jpg') !!}</a>
+          </div>
+        </div>
+      </div> <!-- END RIGHT MENU -->
     </div>
+    <!-- FOOTER -->
+    <div id="footer" class="row"><p>copyright @ 2016</p></div> <!-- END FOOTER -->
 	</div>
 </body>
 </html>
